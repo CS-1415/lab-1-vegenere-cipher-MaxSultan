89e363e2b1b2e6e36debd4869615a62a5e3dbb4d
Max Sultan <maxsultankent@gmail.com>
2026-01-18 15:34:57 -0700

    IsLowercaseLetter tests now pass

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
 ﻿// Max Sultan, January 18th 2026, Lab 1 Venegere Cipher
+
+using System.Diagnostics;
+
+static void TestIsLowercaseLetter() {
+    Debug.Assert(IsLowercaseLetter('a'));
+    Debug.Assert(IsLowercaseLetter('b'));
+    Debug.Assert(IsLowercaseLetter('z'));
+    Debug.Assert(!IsLowercaseLetter('A'));
+    Debug.Assert(!IsLowercaseLetter('`'));
+    Debug.Assert(!IsLowercaseLetter('{'));
+}
+
+static bool IsLowercaseLetter(char c) {
+    return System.Convert.ToInt32(c) >= 97 && System.Convert.ToInt32(c) <= 122;
+}
+
+TestIsLowercaseLetter();
+
 Console.WriteLine("This program encrypts the characters of a message using the Vigenere method.");
 Console.WriteLine("Please enter the message:");
 string message = Console.ReadLine();
